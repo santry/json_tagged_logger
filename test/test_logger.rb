cd36617ce77f32100cf61ab20f610fbc2452c582
Sean Santry <sean@seansantry.com>
2024-04-11 12:16:33 -0400

    Fix setting Formatter#pretty_print from initialize method
    Closes #12
    
    Thanks to @pboling!

--- test/test_logger.rb
+++ test/test_logger.rb
@@ -17,4 +17,9 @@ class LoggerTest < Minitest::Test
   def test_new_creates_tagged_logging_logger
     assert @logger.respond_to?(:tagged)
   end
+
+  def test_new_accepts_pretty_print_option_for_formatter
+    @logger = JsonTaggedLogger::Logger.new(::Logger.new(@output), pretty_print: true)
+    assert @logger.formatter.pretty_print
+  end
 end
