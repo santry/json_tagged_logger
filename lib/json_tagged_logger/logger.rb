6d8acb04754311339cf27ee1a0cad2ab1a6edd0b
Sean Santry <sean@seansantry.com>
2024-10-17 11:54:45 -0400

    Add Rails 5.2 and 6.0 on Ruby 2.6 to CI

--- lib/json_tagged_logger/logger.rb
+++ lib/json_tagged_logger/logger.rb
@@ -4,8 +4,23 @@ require 'active_support/tagged_logging'
 module JsonTaggedLogger
   module Logger
     def self.new(logger, pretty_print: false)
-      logger.formatter = Formatter.new(pretty_print: pretty_print)
-      ActiveSupport::TaggedLogging.new(logger)
+      logger = logger.clone
+      logger.formatter =  JsonTaggedLogger::Formatter.new(pretty_print: pretty_print)
+      logger.extend(self)
     end
+
+    delegate :push_tags, :pop_tags, :clear_tags!, to: :formatter
+
+    def tagged(*tags)
+      if block_given?
+        formatter.tagged(*tags) { yield self }
+      else
+        logger = JsonTaggedLogger::Logger.new(self)
+        logger.formatter = JsonTaggedLogger::Formatter.new
+        logger.push_tags(*formatter.current_tags, *tags)
+        logger
+      end
+    end
+
   end
 end
